6d74898647d54a2741b767a235a111314a38899e
Ben Bowen <bcbowen@gmail.com>
2022-01-10 10:23:54 -0500

    All tests but one passing

--- GraphSearch-ShortestPath-DataStructures/week2/ShortestPath/ShortestPathTests/GraphTests.cs
+++ GraphSearch-ShortestPath-DataStructures/week2/ShortestPath/ShortestPathTests/GraphTests.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 using NUnit.Framework;
 using Graph;
-using Graph.DataStructures;
+
 
 namespace ShortestPathTests
 {
--- GraphSearch-ShortestPath-DataStructures/week2/ShortestPath/ShortestPathTests/TestDataTests.cs
+++ GraphSearch-ShortestPath-DataStructures/week2/ShortestPath/ShortestPathTests/TestDataTests.cs
@@ -11,11 +11,13 @@ namespace ShortestPathTests
     [TestFixture]
     public class TestDataTests
     {
+        private int[] _testIds = { 7, 37, 59, 82, 99, 115, 133, 165, 188, 197 };
+
         [TestCase("input_random_1_4.txt")]
         [TestCase("input_random_2_4.txt")]
         [TestCase("input_random_3_4.txt")]
         [TestCase("input_random_4_4.txt")]
-        public void SmallGraphTests(string fileName) 
+        public void TinyGraphTests(string fileName) 
         {
             FileInfo testFile = GetTestFile(fileName);
             Assert.True(File.Exists(testFile.FullName));
@@ -24,11 +26,143 @@ namespace ShortestPathTests
             graph.CalculateShortestPaths();
             List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
 
-            var result = graph.ExploredNodes.OrderBy(n => n.NodeId).ToList();
+            List<int> result = GetTestResults(_testIds, graph);
             Assert.AreEqual(result.Count(), expectedResult.Count);
             for (int i = 0; i < result.Count(); i++) 
             {
-                Assert.AreEqual(expectedResult[i], result[i].MinDistance);
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+
+        }
+
+        [TestCase("input_random_5_8.txt")]
+        [TestCase("input_random_6_8.txt")]
+        [TestCase("input_random_7_8.txt")]
+        [TestCase("input_random_8_8.txt")]
+        public void SmallGraphTests(string fileName)
+        {
+            FileInfo testFile = GetTestFile(fileName);
+            Assert.True(File.Exists(testFile.FullName));
+
+            UndirectedGraph graph = UndirectedGraph.LoadGraph(testFile.FullName);
+            graph.CalculateShortestPaths();
+            List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
+
+            List<int> result = GetTestResults(_testIds, graph);
+            Assert.AreEqual(result.Count(), expectedResult.Count);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+
+        }
+
+        [TestCase("input_random_9_16.txt")]
+        [TestCase("input_random_10_16.txt")]
+        [TestCase("input_random_11_16.txt")]
+        [TestCase("input_random_12_16.txt")]
+        public void MediumGraphTests(string fileName)
+        {
+            FileInfo testFile = GetTestFile(fileName);
+            Assert.True(File.Exists(testFile.FullName));
+
+            UndirectedGraph graph = UndirectedGraph.LoadGraph(testFile.FullName);
+            graph.CalculateShortestPaths();
+            List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
+
+            List<int> result = GetTestResults(_testIds, graph);
+            Assert.AreEqual(result.Count(), expectedResult.Count);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+
+        }
+
+        [TestCase("input_random_13_32.txt")]
+        [TestCase("input_random_14_32.txt")]
+        [TestCase("input_random_15_32.txt")]
+        [TestCase("input_random_16_32.txt")]
+        public void LargeGraphTests(string fileName)
+        {
+            FileInfo testFile = GetTestFile(fileName);
+            Assert.True(File.Exists(testFile.FullName));
+
+            UndirectedGraph graph = UndirectedGraph.LoadGraph(testFile.FullName);
+            graph.CalculateShortestPaths();
+            List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
+
+            List<int> result = GetTestResults(_testIds, graph);
+            Assert.AreEqual(result.Count(), expectedResult.Count);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+
+        }
+
+        [TestCase("input_random_17_64.txt")]
+        [TestCase("input_random_18_64.txt")]
+        [TestCase("input_random_19_64.txt")]
+        [TestCase("input_random_20_64.txt")]
+        public void LargerGraphTests(string fileName)
+        {
+            FileInfo testFile = GetTestFile(fileName);
+            Assert.True(File.Exists(testFile.FullName));
+
+            UndirectedGraph graph = UndirectedGraph.LoadGraph(testFile.FullName);
+            graph.CalculateShortestPaths();
+            List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
+
+            List<int> result = GetTestResults(_testIds, graph);
+            Assert.AreEqual(result.Count(), expectedResult.Count);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+
+        }
+
+        [TestCase("input_random_21_128.txt")]
+        [TestCase("input_random_22_128.txt")]
+        [TestCase("input_random_23_128.txt")]
+        [TestCase("input_random_24_128.txt")]
+        public void HugeGraphTests(string fileName)
+        {
+            FileInfo testFile = GetTestFile(fileName);
+            Assert.True(File.Exists(testFile.FullName));
+
+            UndirectedGraph graph = UndirectedGraph.LoadGraph(testFile.FullName);
+            graph.CalculateShortestPaths();
+            List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
+
+            List<int> result = GetTestResults(_testIds, graph);
+            Assert.AreEqual(result.Count(), expectedResult.Count);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
+            }
+
+        }
+
+        [TestCase("input_random_25_256.txt")]
+        [TestCase("input_random_26_256.txt")]
+        [TestCase("input_random_27_256.txt")]
+        [TestCase("input_random_28_256.txt")]
+        public void MassiveGraphTests(string fileName)
+        {
+            FileInfo testFile = GetTestFile(fileName);
+            Assert.True(File.Exists(testFile.FullName));
+
+            UndirectedGraph graph = UndirectedGraph.LoadGraph(testFile.FullName);
+            graph.CalculateShortestPaths();
+            List<int> expectedResult = GetOutputs(GetOutputFileName(testFile.FullName));
+
+            List<int> result = GetTestResults(_testIds, graph);
+            Assert.AreEqual(result.Count(), expectedResult.Count);
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expectedResult[i], result[i]);
             }
 
         }
@@ -65,10 +199,15 @@ namespace ShortestPathTests
             return outputs;
         }
 
-        private List<int> GetTestResults(DirectedGraph graph) 
+        private List<int> GetTestResults(int[] nodeIds, UndirectedGraph graph) 
         {
-
             List<int> results = new List<int>();
+            foreach (int id in nodeIds) 
+            {
+                results.Add(graph.ExploredNodes.First(n => n.NodeId == id).MinDistance);
+            }
+
+            return results;
         }
     }
 }
