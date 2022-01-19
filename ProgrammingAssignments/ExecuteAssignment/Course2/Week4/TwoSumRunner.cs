00c5a16dc8b266fc0b3b469a6203f8ead5ebe0bb
Ben Bowen <bcbowen@gmail.com>
2022-01-18 23:35:41 -0500

    Finished PA 4 course 2

---
+++ ProgrammingAssignments/ExecuteAssignment/Course2/Week4/TwoSumRunner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+using Algorithms.HashTable;
+
+namespace ExecuteAssignment.Course2.Week4
+{
+    internal static class TwoSumRunner
+    {
+        public static int Run() 
+        {
+            Console.WriteLine("Running Two Sum Algorithm");
+            
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Course2", "Week4", "PAData.txt");
+
+            int value;
+
+            return TwoSum.GetSums(path);
+        }
+    }
+}
--- ProgrammingAssignments/ExecuteAssignment/Program.cs
+++ ProgrammingAssignments/ExecuteAssignment/Program.cs
@@ -12,6 +12,7 @@ namespace ExecuteAssignment
                 Console.WriteLine("Choose a numeric option, or anything else to exit");
 
                 Console.WriteLine("7. Median Maintenance (Course 2 week 3)");
+                Console.WriteLine("8. Two Sum (Course 2 week 4)");
                 ConsoleKeyInfo k = Console.ReadKey();
                 Console.WriteLine();
 
@@ -20,6 +21,10 @@ namespace ExecuteAssignment
                     case '7':
                         Course2.Week3.MedianMaintenanceRunner.Run();
                         Console.WriteLine();
+                        break;
+                    case '8':
+                        int result = Course2.Week4.TwoSumRunner.Run();
+                        Console.WriteLine($"Result: {result}");
                         break;
                     default:
                         Console.WriteLine("Selected key doesn't match choice, we're outta here");
