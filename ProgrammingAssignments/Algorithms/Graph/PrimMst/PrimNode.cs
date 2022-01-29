7de6b38d87b09bb78620fbf73bd0927988deec41
Ben Bowen <bcbowen@gmail.com>
2022-01-29 17:51:59 -0500

    WIP: worked on Prim MST

---
+++ ProgrammingAssignments/Algorithms/Graph/PrimMst/PrimNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Algorithms.Shared;
+
+namespace Algorithms.Graph.PrimMst
+{
+    public class PrimNode : Node
+    {
+        public PrimNode(int value, int nodeId, int referencedNode) : base(value)
+        { 
+            NodeId = nodeId;
+            ReferencedNode = referencedNode;
+        }
+
+        public int NodeId { get; set; }
+        public int ReferencedNode { get; set; }
+    }
+}
