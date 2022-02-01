f1c40e3b2d4395c8afcce435b4d7d64ffe179d76
Ben Bowen <bcbowen@gmail.com>
2022-02-01 15:48:59 -0500

    Added remaining test cases for edge clustering

--- ProgrammingAssignments/Algorithms/Tests/Greedy/Clustering/EdgeClusteringTests.cs
+++ ProgrammingAssignments/Algorithms/Tests/Greedy/Clustering/EdgeClusteringTests.cs
@@ -14,7 +14,58 @@ namespace Algorithms.Tests.Greedy.Clustering
     public class EdgeClusteringTests
     {
         [TestCase(1, 8)]
+        [TestCase(2, 8)]
+        [TestCase(3, 8)]
+        [TestCase(4, 8)]
         public void EdgeClusteringTestTiny(int testId, int nodeCount) 
+        {
+            RunTest(testId, nodeCount);
+        }
+
+        [TestCase(5, 16)]
+        [TestCase(6, 16)]
+        [TestCase(7, 16)]
+        [TestCase(8, 16)]
+        [TestCase(9, 32)]
+        [TestCase(10, 32)]
+        [TestCase(11, 32)]
+        [TestCase(12, 32)]
+        public void EdgeClusteringTestSmall(int testId, int nodeCount)
+        {
+            RunTest(testId, nodeCount);
+        }
+
+        [TestCase(13, 64)]
+        [TestCase(14, 64)]
+        [TestCase(15, 64)]
+        [TestCase(16, 64)]
+        [TestCase(17, 128)]
+        [TestCase(18, 128)]
+        [TestCase(19, 128)]
+        [TestCase(20, 128)]
+        public void EdgeClusteringTestMedium(int testId, int nodeCount)
+        {
+            RunTest(testId, nodeCount);
+        }
+
+        [TestCase(21, 256)]
+        [TestCase(22, 256)]
+        [TestCase(23, 256)]
+        [TestCase(24, 256)]
+        [TestCase(25, 512)]
+        [TestCase(26, 512)]
+        [TestCase(27, 512)]
+        [TestCase(28, 512)]
+        public void EdgeClusteringTestLarge(int testId, int nodeCount)
+        {
+            RunTest(testId, nodeCount);
+        }
+
+        [TestCase(29, 1024)]
+        [TestCase(30, 1024)]
+        [TestCase(31, 1024)]
+        [TestCase(32, 1024)]
+        public void EdgeClusteringTestXL(int testId, int nodeCount)
         {
             RunTest(testId, nodeCount);
         }
