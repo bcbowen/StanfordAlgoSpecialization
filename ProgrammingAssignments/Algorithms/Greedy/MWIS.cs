1348915ba35c01853d629574b815e474350a1616
Ben Bowen <bcbowen@gmail.com>
2022-02-13 21:24:54 -0500

    Some MWIS tests passing

---
+++ ProgrammingAssignments/Algorithms/Greedy/MWIS.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Greedy
+{
+    public static class MWIS
+    {
+        private static int[] _checkNodes = { 1, 2, 3, 4, 17, 117, 517, 997 };
+        // 1, 2, 3, 4, 17, 117, 517, and 997
+        public static string RunAlgorithm(string fileName) 
+        {
+            int[] nodes = LoadNodes(fileName);
+            Dictionary<int, int> maxPath = new Dictionary<int, int>();
+            int i = nodes.Length;
+            StringBuilder result = new StringBuilder();
+            while (i >= 1) 
+            {
+                if (nodes[i - 1] >= nodes[i - 2]) 
+                {
+                    i -= 1;
+                }
+                else 
+                {
+                    maxPath.Add(i + 1, nodes[i - i]);
+                    i -= 2;
+                }
+            }
+            if (i == 0) 
+            {
+                maxPath.Add(i + 1, nodes[i]);
+            }
+
+            foreach (int checkNode in _checkNodes) 
+            {
+                if (maxPath.ContainsKey(checkNode))
+                {
+                    result.Append('1');
+                }
+                else 
+                {
+                    result.Append('0');
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static int[] LoadNodes(string fileName)
+        {
+            List<int> nodes = new List<int>();
+            using (StreamReader reader = new StreamReader(fileName)) 
+            {                
+                string line = reader.ReadLine();
+                int nodeCount = int.Parse(line);
+                for (int i = 0; i < nodeCount; i++) 
+                {
+                    line = reader.ReadLine();
+                    nodes.Add(int.Parse(line));
+                }
+
+                reader.Close();
+            }
+            return nodes.ToArray();
+        }
+    }
+}
---
+++ ProgrammingAssignments/Algorithms/Tests/Greedy/MWIS/MWISTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Algorithms.Greedy;
+
+namespace Algorithms.Tests.Greedy.MWIS
+{
+    [TestFixture]
+    public class MWISTests
+    {
+
+        [Test]
+        public void TestFileLoaded()
+        {
+            string fileName = $"input_random_1_10.txt";
+            DirectoryInfo testDirectory = TestUtils.GetTestCaseDirectory().GetDirectories("MWISData").First();
+            FileInfo file = testDirectory.GetFiles(fileName).FirstOrDefault();
+
+            int[] nodes = Algorithms.Greedy.MWIS.LoadNodes(file.FullName);
+            Assert.AreEqual(10, nodes.Length);
+        }
+
+        [TestCase(1, 10)]
+        [TestCase(2, 10)]
+        [TestCase(3, 10)]
+        [TestCase(4, 10)]
+        public void MWISTestsTiny(int testNumber, int count) 
+        {
+            RunTest(testNumber, count);
+        }
+
+        [TestCase(5, 20)]
+        [TestCase(6, 20)]
+        [TestCase(7, 20)]
+        [TestCase(8, 20)]
+        [TestCase(9, 40)]
+        [TestCase(10, 40)]
+        [TestCase(11, 40)]
+        [TestCase(12, 40)]
+        public void MWISTestsSmall(int testNumber, int count)
+        {
+            RunTest(testNumber, count);
+        }
+
+        private void RunTest(int testNumber, int count)
+        {
+            string fileName = $"input_random_{testNumber}_{count}.txt";
+            DirectoryInfo testDirectory = TestUtils.GetTestCaseDirectory().GetDirectories("MWISData").First();
+            FileInfo file = testDirectory.GetFiles(fileName).FirstOrDefault();
+            Assert.NotNull(file, "Test file not found");
+
+            string result = Algorithms.Greedy.MWIS.RunAlgorithm(file.FullName);
+            string expectedOutput = GetExpectedOutput(file.FullName);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        private string GetExpectedOutput(string fileName)
+        {
+            string outputFileName = fileName.Replace("input_", "output_");
+            string output; 
+            using (StreamReader reader = new StreamReader(outputFileName))
+            {
+                output = reader.ReadLine();
+                reader.Close();
+            }
+
+            return output;
+        }
+    }
+}
