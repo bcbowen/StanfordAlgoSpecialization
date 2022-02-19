718f7e362c36fe49abaf067b975794279f13d1ae
Ben Bowen <bcbowen@gmail.com>
2022-02-19 18:40:41 -0500

    Start setting up knapsack

---
+++ ProgrammingAssignments/Algorithms/Greedy/Knapsack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Algorithms.Shared;
+
+namespace Algorithms.Greedy
+{
+    public static class Knapsack
+    {
+        public static int RunAlgorithm(List<Node> nodes, int capacity) 
+        {
+            return 42;
+        }
+
+        public static int RunAlgorithm(string fileName) 
+        {
+            (List<Node> nodes, int capacity) = LoadKnapsackData(fileName);
+            return RunAlgorithm(nodes, capacity);
+        }
+
+        public static (List<Node> nodes, int capacity) LoadKnapsackData(string fileName) 
+        {
+            int capacity = 0;
+            List<Node> nodes = new List<Node>();
+
+            using (StreamReader reader = new StreamReader(fileName)) 
+            {
+                string[] fields;
+                // first line has capacity and number of items (count)
+                string line = reader.ReadLine();
+                fields = line.Split(' ');
+                capacity = int.Parse(fields[0]);
+                while ((line = reader.ReadLine()) != null)
+                {
+                    fields = line.Split(' ');
+                    nodes.Add(new Node(int.Parse(fields[0]), int.Parse(fields[1])));
+                }
+                reader.Close();
+            }
+            return (nodes, capacity);
+        }
+    }
+}
---
+++ ProgrammingAssignments/Algorithms/Tests/Greedy/Knapsack/KnapsackTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Algorithms.Shared;
+using Algorithms.Greedy;
+
+namespace Algorithms.Tests.Greedy.Knapsack
+{
+    [TestFixture]
+    public class KnapsackTests
+    {
+        const string DataFileName = "KnapsackData";
+
+        [Test]
+        public void TestNodesLoadedFromFile()
+        {
+            string fileName = $"input_random_17_100_1000.txt";
+            DirectoryInfo testDirectory = TestUtils.GetTestCaseDirectory().GetDirectories(DataFileName).First();
+            FileInfo file = testDirectory.GetFiles(fileName).FirstOrDefault();
+
+            (List<Node> nodes, int capacity) = Algorithms.Greedy.Knapsack.LoadKnapsackData(file.FullName);
+            Assert.AreEqual(1000, nodes.Count);
+            Assert.AreEqual(100, capacity);
+        }
+
+    }
+}
