b0331fddc560af0248f7f9cd1c60ceaaac535034
Ben Bowen <bcbowen@gmail.com>
2022-03-19 15:07:08 -0400

    Added all test cases, tiny and 2 smalls are passing, the rest are off

--- ProgrammingAssignments/Algorithms/Tests/Plane/TravelingSalesmanHeuristicTests.cs
+++ ProgrammingAssignments/Algorithms/Tests/Plane/TravelingSalesmanHeuristicTests.cs
@@ -137,6 +137,100 @@ namespace Algorithms.Tests.Plane
             RunTSPHeuristic(testNumber, count);
         }
 
+        [TestCase(9, 8)]
+        [TestCase(10, 8)]
+        [TestCase(11, 8)]
+        [TestCase(12, 8)]
+        [TestCase(13, 10)]
+        [TestCase(14, 10)]
+        [TestCase(15, 10)]
+        [TestCase(16, 10)]
+        public void CalculateShortestPathSmallFiles(int testNumber, int count)
+        {
+            RunTSPHeuristic(testNumber, count);
+        }
+
+        [TestCase(17, 20)]
+        [TestCase(18, 20)]
+        [TestCase(19, 20)]
+        [TestCase(20, 20)]
+        [TestCase(21, 40)]
+        [TestCase(22, 40)]
+        [TestCase(23, 40)]
+        [TestCase(24, 40)]
+        public void CalculateShortestPathMediumFiles(int testNumber, int count)
+        {
+            RunTSPHeuristic(testNumber, count);
+        }
+
+        [TestCase(25, 80)]
+        [TestCase(26, 80)]
+        [TestCase(27, 80)]
+        [TestCase(28, 80)]
+        [TestCase(29, 100)]
+        [TestCase(30, 100)]
+        [TestCase(31, 100)]
+        [TestCase(32, 100)]
+        [TestCase(33, 200)]
+        [TestCase(34, 200)]
+        [TestCase(35, 200)]
+        [TestCase(36, 200)]
+        [TestCase(37, 400)]
+        [TestCase(38, 400)]
+        [TestCase(39, 400)]
+        [TestCase(40, 400)]
+        public void CalculateShortestPathLargeFiles(int testNumber, int count)
+        {
+            RunTSPHeuristic(testNumber, count);
+        }
+
+        [TestCase(41, 800)]
+        [TestCase(42, 800)]
+        [TestCase(43, 800)]
+        [TestCase(44, 800)]
+        [TestCase(45, 1000)]
+        [TestCase(46, 1000)]
+        [TestCase(47, 1000)]
+        [TestCase(48, 1000)]
+        [TestCase(49, 2000)]
+        [TestCase(50, 2000)]
+        [TestCase(51, 2000)]
+        [TestCase(52, 2000)]
+        [TestCase(53, 4000)]
+        [TestCase(54, 4000)]
+        [TestCase(55, 4000)]
+        [TestCase(56, 4000)]
+        public void CalculateShortestPathXLFiles(int testNumber, int count)
+        {
+            RunTSPHeuristic(testNumber, count);
+        }
+
+        [TestCase(57, 8000)]
+        [TestCase(58, 8000)]
+        [TestCase(59, 8000)]
+        [TestCase(60, 8000)]
+        [TestCase(61, 10000)]
+        [TestCase(62, 10000)]
+        [TestCase(63, 10000)]
+        [TestCase(64, 10000)]
+        [TestCase(65, 20000)]
+        [TestCase(66, 20000)]
+        [TestCase(67, 20000)]
+        [TestCase(68, 20000)]
+        public void CalculateShortestPathHugeFiles(int testNumber, int count)
+        {
+            RunTSPHeuristic(testNumber, count);
+        }
+
+        [TestCase(69, 40000)]
+        [TestCase(70, 40000)]
+        [TestCase(71, 40000)]
+        [TestCase(72, 40000)]
+        public void CalculateShortestPathGinormousFiles(int testNumber, int count)
+        {
+            RunTSPHeuristic(testNumber, count);
+        }
+
         /// <summary>
         /// Run the Traveling Salesman Heuristic algorithm
         /// </summary>
