70e9d4c6f5560d0911e1a093263d842f6b9ae1b9
Ben Bowen <bcbowen@gmail.com>
2022-03-18 22:51:04 -0400

    [WIP] Starting algo for tsp

---
+++ ProgrammingAssignments/Algorithms/Plane/TSPHeuristic/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Plane.TSPHeuristic
+{
+    public class Point
+    {
+        public Point(int index, double x, double y)
+        {
+            Index = index;
+            X = x;
+            Y = y;
+        }
+        
+        public int Index { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point NearestNeighbor { get; private set; }
+
+        public double NearestNeighborDistance { get; private set; }
+        public void SetNearestNeighbor(Point neighbor)
+        {
+            NearestNeighbor = neighbor;
+
+            double distance = System.Math.Sqrt(System.Math.Pow(neighbor.X - X, 2) + System.Math.Pow(neighbor.Y - Y, 2));
+            NearestNeighborDistance = distance;
+        }
+    }
+}
