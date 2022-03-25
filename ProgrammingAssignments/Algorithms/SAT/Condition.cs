b747ddddd7ce92a57449734c3a766d0365351339
Ben Bowen <bcbowen@gmail.com>
2022-03-24 23:48:53 -0400

    Started work on 2 sat: : :

---
+++ ProgrammingAssignments/Algorithms/SAT/Condition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.SAT
+{
+    public class Condition
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+
+        public bool Is1 { get; set; }
+
+        public bool Is2 { get; set; }
+
+        public static Condition Parse(string line)
+        {
+            Condition condition = new Condition();
+            string[] parts = line.Split(' ');
+            if (parts.Length > 1)
+            {
+                int val = int.Parse(parts[0]);
+                condition.Is1 = val > 0;
+                condition.Value1 = System.Math.Abs(val);
+
+                val = int.Parse(parts[1]);
+                condition.Is2 = val > 0;
+                condition.Value2 = System.Math.Abs(val);
+            }
+            else
+            {
+                throw new ArgumentException("Line does not have the expected format.. expected space delimited pair of ints, man", nameof(line));
+            }
+
+            return condition;
+        }
+    }
+}
---
+++ ProgrammingAssignments/Algorithms/Tests/TwoSat/TwoSatTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Algorithms.SAT;
+
+namespace Algorithms.Tests.TwoSat
+{
+    [TestFixture]
+    public class TwoSatTests
+    {
+        [TestCase("1 2", 1, true, 2, true)]
+        [TestCase("-1 2", 1, false, 2, true)]
+        [TestCase("1 -2", 1, true, 2, false)]
+        [TestCase("-1 -2", 1, false, 2, false)]
+        public void ConditionParsingTests(string line, int expectedValue1, bool expectedIs1, int expectedValue2, bool expectedIs2) 
+        { 
+            Condition condition = Condition.Parse(line);
+            Assert.AreEqual(expectedValue1, condition.Value1);
+            Assert.AreEqual(expectedValue2, condition.Value2);
+            Assert.AreEqual(expectedIs1, condition.Is1);
+            Assert.AreEqual(expectedIs2, condition.Is2);
+        }
+
+    }
+}
